28fd1bb158d9c918e18e87cec93bfc92b5a3d3c1
lukedurrant <contact@lukedurrant.com>
2019-02-21 23:42:43 +1100

    tvOS Support

---
+++ InAppPurchaseButton tvOS/InAppPurchaseButton_tvOS.h
@@ -0,0 +1,18 @@
+//
+//  InAppPurchaseButton_tvOS.h
+//  InAppPurchaseButton tvOS
+//
+//  Created by Luke Durrant on 21/2/19.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for InAppPurchaseButton_tvOS.
+FOUNDATION_EXPORT double InAppPurchaseButton_tvOSVersionNumber;
+
+//! Project version string for InAppPurchaseButton_tvOS.
+FOUNDATION_EXPORT const unsigned char InAppPurchaseButton_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InAppPurchaseButton_tvOS/PublicHeader.h>
+
+
